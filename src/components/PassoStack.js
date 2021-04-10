c106593655f9164d31e2b7153e5cfb1b32ec08bb
carlos.guimaraes <carlosh.guimaraes@hotmail.com>
2021-04-09 23:12:31 -0300

    this is a teste

--- src/components/PassoStack.js
+++ src/components/PassoStack.js
@@ -4,8 +4,8 @@ import { View, Button } from 'react-native';
 export default props => (
         <View style={{ flex: 1 }}>
             <View style={{ flexDirection: 'row', justifyContent: 'space-around'}}>
-                {props.voltar? <Button title="Voltar" onPress={() => { props.navigation.goBack()} }/> : false }
-                {props.avancar? <Button title="Avancar" onPress={() => { props.navigation.navigate(props.avancar)} }/> : false }
+                {props.voltar? <Button title="Voltare" onPress={() => { props.navigation.goBack()} }/> : false }
+                {props.avancar? <Button title="Avancare" onPress={() => { props.navigation.navigate(props.avancar)} }/> : false }
             </View>
             <View style={{ flex: 1 }}>
                 {props.children}
